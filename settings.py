1d3c2f4e552b3bb17f557d8ad6694b0f0ab234d2
notapresent <notapresent@gmail.com>
2018-01-11 00:09:48 +0000

    Set concurrency via settings

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ import logging
 
 # Required settings
 FORUM_URL = os.environ['FORUM_URL']
-CONCURRENCY = 16
 STORAGE_URL = os.environ['STORAGE_URL']
 
 # Optional settings with sensible defaults
+CONCURRENCY = int(os.environ.get('CONCURRENCY', 16))
 LOG_LEVEL = getattr(logging, os.environ.get('LOG_LEVEL', 'INFO'), logging.INFO)
 HTML_ENCODING = os.environ.get('HTML_ENCODING')     # None means autodetect (slower)
 STORAGE_CURLOPTS = os.environ.get('STORAGE_CURLOPTS', '')
